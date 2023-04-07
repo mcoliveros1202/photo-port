fc6fb4f9de4537434def999fbbb9f37cc33cac3d
moliveros <mcoliveros1202@gmail.com>
2023-04-07 13:15:00 -0500

    created nav bar with categories

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import Nav from './components/Nav';
+import About from './components/About';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     
     <div>
+      <Nav></Nav>
+      <main>
+        <About></About>
+      </main>
     </div>
   );
 }
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,13 @@
+import coverImage from "../../assets/cover/cover-image.jpg";
 import React from 'react';
 
-const About = ()=> {
-    <section>
-        <h1>Who Am I?</h1>
-    </section>
+const About = () => {
+    return (
+        <section className='my-5'>
+            <h1 id='about'>Who Am I?</h1>
+            <img src={coverImage} className='my-2' style={{ width: '100%' }} alt='cover' />
+        </section>
+    );
 }
 
 export default About;
